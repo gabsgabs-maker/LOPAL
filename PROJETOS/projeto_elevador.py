df08615cbf1c33286fcd63207ccd378ee36d8d03
Gabrielli Araujo <gabrielliaraujo509@gmail.com>
2026-05-08 11:34:25 -0300

    projeto elevador

---
+++ PROJETOS/projeto_elevador.py
@@ -0,0 +1,24 @@
+#Sistema de elevador de prédio
+#O prédio possui 10 andares, sendo o térreo o andad 0. O elevador pode se mover para cima ou para baixo, e tem capacidade de transportar até 5 pessoas.
+#O elevador começa no andar 0 e pode ser chamado por qualquer pessoa e qualquer andar.
+#O elevador deve se mover para onde a pessoa chamou, e depois para o andar destino da pessoa.
+#O elevador deve exibir mensagens indicando o andar atual, o número de pessoas no elevador, e as ações realizadas (subindo, descendo, parando). O programa deve continuar rodando até que o usuário decida encerrar.
+
+ #Requisitos funcionais:
+#  O elevador deve se mover para cima e para baixo
+#  O elevador deve se dirigir no andar que a pessoa escolheu
+#  O elevador deve informaas açoes
+# O programa deve permitir várias chamadas do elevador.
+# O sistema deve encerrar apenas quando o usuário escolher sair.
+# Requisitos Não Funcionais
+
+# O sistema deve ser fácil de usar.
+# As mensagens exibidas devem ser claras.
+# O programa deve responder rapidamente aos comandos.
+# O sistema não deve permitir mais de 5 pessoas no elevador.
+# O sistema não deve permitir andares menores que 0.
+# O sistema não deve permitir andares maiores que 10.
+# O programa deve funcionar continuamente até o usuário encerrar.
+# O sistema deve evitar erros de entrada inválida.
+# O código deve ser organizado e fácil de manter.
+
